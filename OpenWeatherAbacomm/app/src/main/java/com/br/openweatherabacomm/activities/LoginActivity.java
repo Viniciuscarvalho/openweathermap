ed7ed5fde522f975aeedb9fd99a9c99078885a4d
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2015-12-26 15:14:09 -0300

    Login with Facebook.

--- OpenWeatherAbacomm/app/src/main/java/com/br/openweatherabacomm/activities/LoginActivity.java
+++ OpenWeatherAbacomm/app/src/main/java/com/br/openweatherabacomm/activities/LoginActivity.java
@@ -1,21 +1,107 @@
 package com.br.openweatherabacomm.activities;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.br.openweatherabacomm.R;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String AUTH_TOKEK_TYPE = "auth";
+    private static final String USER_ID = "id";
+    private static final String USER_NAME = "name";
+    private static final String USER_EMAIL = "email";
+    private static final String USER_PROFILE_PHOTO = "profilePhoto";
+    private static final String USER_COVE_PHOTO = "coverPhoto";
+    private static final String USER_FRIENDS_TO = "friendsId";
+
+    private static final String ARGS = "args";
+    private static final String ARG_USER = "user";
+
+    private TextView mTextDetails;
+
+    private CallbackManager mCallbackManager;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+       getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle("");
+        mProgressDialog.setMessage("Carregando");
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setCancelable(false);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook);
+        loginButton.setReadPermissions(Arrays.asList("email", "public_profile", "user_friends"));
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mProgressDialog.show();
+                AccessToken accessToken = loginResult.getAccessToken();
+
+                startMainActivity();
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("WeatherAbacomm", "Login with Facebook canceled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i("WeatherAbacomm", "Error login with Facebook", error);
+
+            }
+       });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void startMainActivity(){
+        Bundle args = new Bundle();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        onBackPressed();
+    }
+
+
 
 }
